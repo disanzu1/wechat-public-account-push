4ae49d9ea96533ca796b2b6b5833a50b29a01dec
disanzu1 <67783705+disanzu1@users.noreply.github.com>
2023-01-11 10:08:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'B-XS2ZP7_QasLjmpbdqxwTVHwdeQRI6_PQo-4WpaFrw,
+  CALLBACK_TEMPLATE_ID: 'B-XS2ZP7_QasLjmpbdqxwTVHwdeQRI6_PQo-4WpaFrw‘,
 
   CALLBACK_USERS: [
     {
